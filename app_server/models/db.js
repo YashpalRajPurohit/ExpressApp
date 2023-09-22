d15a7fbe904808ca234957069ca78384a786005b
Yashpal Singh Raj Purohit <yashpalsinghrajpurohith2003@gmail.com>
2023-09-22 14:08:45 +0530

    updated models and server for MongoDB

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
-const dbURI = 'mongodb://localhost/Loc8r';
 
+const dbURI = 'mongodb://0.0.0.0:27017/Loc8r';
 mongoose.connect(dbURI, { useNewUrlParser: true });
+
 mongoose.connection.on('connected', () => {
     console.log(`Mongoose connected to ${dbURI}`);
 });
+
 mongoose.connection.on('error', err => {
-    console.log(`Mongoose connection error: ${err}`);
+    console.log('Mongoose connection error:', err);
 });
+
 mongoose.connection.on('disconnected', () => {
     console.log('Mongoose disconnected');
 });
@@ -18,12 +21,14 @@ const gracefulShutdown = (msg, callback) => {
         callback();
     });
 };
+
 // For nodemon restarts
 process.once('SIGUSR2', () => {
     gracefulShutdown('nodemon restart', () => {
         process.kill(process.pid, 'SIGUSR2');
     });
 });
+
 // For app termination
 process.on('SIGINT', () => {
     gracefulShutdown('app termination', () => {
