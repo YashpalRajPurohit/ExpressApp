fae1bd8203046d8c5d21c494652f9a5291c850e0
Yashpal Singh Raj Purohit <yashpalsinghrajpurohith2003@gmail.com>
2023-09-29 03:28:53 +0530

    updated till API's

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 const indexRouter = require('./app_server/routes/index');
-const usersRouter = require('./app_server/routes/users');
+const routesApi = require('./app_api/routes/index'); // Moved this line up
+const usersRouter = require('./app_server/routes/users'); // Add this line
 
 const app = express();
 
@@ -21,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter); // Use the usersRouter here
+
+app.use('/api', routesApi); // Moved this line down
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -29,7 +32,7 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res) {
+app.use(function (err, req, res, next) { // Add 'next' as a parameter here
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
